6a7abcdd3e28549ac4ba9d729ff192fff2829600
sophoniyas <sophoniyasbikessgn@gmail.com>
2022-12-11 21:24:20 -0800

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - prints if the number is positive,negative or 0
  * Return: Always 0
@@ -9,10 +8,8 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() RAND_MAX / 2;
-
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -25,6 +22,5 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-
 	return (0);
 }
--- 0x01-variables_if_else_while/README.m
+++
@@ -1,3 +0,0 @@
-this executable file creates an alias with a name of ls and value to rm
-
-
