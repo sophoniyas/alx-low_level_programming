02f4263a19bc39338fcdfba626aadae2bda84ef8
sophoniyas <sophoniyasbikessgn@gmail.com>
2022-12-12 11:52:14 -0800

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 /**
@@ -8,36 +9,24 @@
 
 int main(void)
 {
-	int tho;
-	int hun;
-	int ten;
-	int bas;
+	int n;
+	int i = 0;
 
-for ( tho = 0 ; tho < 10 ; tho++)
-{	
-
-	for (hun = 0 ; hun <= 10 ; hun++)
+	for (n = 0 ; n < 100 ; n++)
 	{
-	
-		for (ten = 0 ; ten < 10 ; ten++)
+		putchar(n / 10 + '0');
+		putchar(i + '0');
+		if (n < 99)
 		{
-			for ( bas = 0 ; bas < 10 ; bas++)
-			{
-				putchar('0' + tho);
-				putchar('0' + hun);
-				putchar(32);
-				putchar('0' + ten);
-				putchar('0' + bas);
-				if (!(tho == 9 && hun == 8))
-				{
-					putchar(',');
-					putchar(32);
-				}
-				bas++;
-			}
+			putchar(',');
+			putchar(32);
+		}
+		i++;
+		if (i > 9)
+		{
+			i = 0;
 		}
 	}
-}
 	putchar('\n');
 	return (0);
 }
