acda9febbc790333b5088ebbe4524ddfd6904fc0
sophoniyas <sophoniyasbikessgn@gmail.com>
2022-12-14 06:16:00 -0800

    fixe

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,12 @@
 #include "main.h"
-
 /**
- * main - prints _putchar and a new line
+ * main - Entry point
  *
- * Return: Always 0 
+ * Return: Alway 0 (Sucess)
  */
+
 int main(void)
 {
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,6 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
