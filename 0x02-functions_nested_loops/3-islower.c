9dedbb9abad454197587350bd93f65ff637e5c53
sophoniyas <sophoniyasbikessgn@gmail.com>
2022-12-14 09:48:32 -0800

    checks for alphabetic character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ * @c: character to be checked
+ * Return: 1 for lowaercase character or 0 for anything else
+ */
+int _islower(char c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		
+	return (1);
+	}
+	return (0);
+}
