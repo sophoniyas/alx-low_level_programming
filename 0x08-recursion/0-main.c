1988349f3412d0c50dc39f7c46a94a76767798d1
sophoniyas <sophoniyasbikessgn@gmail.com>
2023-01-04 03:58:20 -0800

    a function that prints a string, followed by a new line

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *  * main - check the code for alx  School students.
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+		_puts_recursion("Puts with recursion");
+			return (0);
+}
