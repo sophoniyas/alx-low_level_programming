d16c19ecfad191096a259438b4f38d1e3d00eb7d
sophoniyas <sophoniyasbikessgn@gmail.com>
2023-01-18 11:40:14 -0800

    a function that prints a name

--- 0-print_name.c
+++ 0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
@@ -7,8 +8,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (!name || !f)
 		return;
-
 	f(name);
 }
